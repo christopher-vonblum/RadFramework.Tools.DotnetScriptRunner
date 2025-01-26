699f9f359ca503ef5c942aaee57b8c8996e2bbed
Christopher von Blum <christopher.vonblum@gmail.com>
2025-01-26 13:28:59 +0100

    done

--- DotnetScriptRunner/Program.cs
+++ DotnetScriptRunner/Program.cs
@@ -1,9 +1,20 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using System.Security.Cryptography;
 
-string scriptFile = args[0];
+string scriptFile;
+
+if (Debugger.IsAttached)
+{
+    scriptFile = "TestScript.csx";
+}
+else
+{
+    scriptFile = args[0];
+}
 
 string scriptName = Path.GetFileName(scriptFile);
+string scriptRootPath = Path.GetPathRoot(scriptFile);
 
 CreateDirectory("compilations");
 
@@ -13,15 +24,17 @@ byte[] script = File.ReadAllBytes(scriptFile);
 
 string scriptVersionHash = Convert.ToHexString(SHA1.HashData(script));
 
-string workingDir = $"compilations/{scriptName}/{scriptVersionHash}";
+string workingDir = System.IO.Path.GetDirectoryName(
+    System.Reflection.Assembly.GetExecutingAssembly().Location) + $"/compilations/{scriptName}/{scriptVersionHash}";
 
 CreateDirectory(workingDir);
 
-RunShellCommand(workingDir, "dotnet", $"new console");
-
+File.Copy("res/script-project.xml", workingDir + "/script.csproj");
 File.WriteAllBytes(workingDir + "/Program.cs", script);
 
-RunShellCommand(workingDir, "dotnet", $"run");
+RunShellCommand(workingDir, "dotnet", "build script.csproj");
+
+RunShellCommand(workingDir, "dotnet", "run bin/Debug/net9.0/script.dll " + string.Join(' ', args.Skip(1)));
 
 void CreateDirectory(string path)
 {
@@ -34,10 +47,17 @@ void CreateDirectory(string path)
 void RunShellCommand(string workingDir, string command, string arguments)
 {
     Process process = new Process();
+    process.StartInfo.UseShellExecute = false;
     process.StartInfo.WorkingDirectory = workingDir;
     process.StartInfo.FileName = command;
-    process.StartInfo.Arguments = command;
+    process.StartInfo.Arguments = arguments;
 
+    process.StartInfo.RedirectStandardOutput = true;
+    process.StartInfo.RedirectStandardError = true;
+    
     process.Start();
     process.WaitForExit();
+
+    Console.WriteLine(process.StandardOutput.ReadToEnd());
+    ;
 }
--- DotnetScriptRunner/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/anon/Documents/workspace/DotnetScriptRunner/DotnetScriptRunner/bin/Debug/net9.0
-
-./DotnetScriptRunner '/home/anon/bin/BashRcDefinitionsUpdater.cs'
